ff3deb2560975eac4dd9823171f7457fe745b36f
otaiga <karl.turner5@gmail.com>
2012-12-31 13:51:02 +0000

    removed redundant route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Comingsoon::Application.routes.draw do
   root :to => 'main#index'
 
   get "results" => 'main#results'
-  get "film_info" => 'main#film_info'
 
   resources :reminders, :only => :create
 
