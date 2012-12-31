aa6aadc345ee8ce6f97cb6a98374a4f44716c013
otaiga <karl.turner5@gmail.com>
2012-12-31 13:41:50 +0000

    updated view to show grouped by dates, updated model to include extra methods for returning grouped hash object.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,8 +8,7 @@ class MainController < ApplicationController
     month = params[:date][:month]
     year = params[:date][:year]
     selection = Date.parse("01-#{month}-#{year}")
-    @response = MovieData.where(release_date: selection.beginning_of_month..selection.end_of_month)
-    ## need to break this down further based on the release date (multiple entries)
+    @response = MovieData.get_dates(selection)
   end
 
 end
