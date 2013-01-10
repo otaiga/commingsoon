a71c8f04bf3acc723f0601ffd6b5136153ff7efa
otaiga <karl.turner5@gmail.com>
2013-01-10 08:23:30 +0000

    blocked out some tests -  in prep

--- spec/mailers/moviereminder_spec.rb
+++ spec/mailers/moviereminder_spec.rb
@@ -1,5 +1,12 @@
 require "spec_helper"
 
 describe Moviereminder do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should send reminder" do
+    movie_name = "This Movie"
+    recipient = "test.mail@testing.com"
+    Moviereminder.send_reminder(movie_name, recipient)
+    ActionMailer::Base.deliveries.last.to.should == ["test.mail@testing.com"]
+    ActionMailer::Base.deliveries.last.subject.should == "Your Movie Reminder for Tomorrow's Release"
+  end
 end
