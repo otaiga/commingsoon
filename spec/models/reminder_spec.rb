9f37154316a95151f15028e8d2de0c9ae6cbdf2b
otaiga <karl.turner5@gmail.com>
2013-01-15 07:47:02 +0000

    updated reminder model to include a dependent destroy on user and included tests

--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -3,21 +3,38 @@ require 'spec_helper'
 describe Reminder do
 
   describe "Valid parameters" do
+    before(:each) do
+      create_confirm_user
+    end
     
     it "should save a reminder for user" do
-      pending ("needs tests")
+      @reminder = given_user.reminders.new(movie_title_id: given_movie.title_id)
+      @reminder.save.should == true
+      given_user.reminders.count.should == 1
     end
 
     it "should destroy a reminder for user" do
-      pending ("needs tests")
+      create_reminders(given_user, given_movie)
+      given_reminder.destroy
+      given_user.reminders.count.should == 0
     end
 
     it "should get the movie profile of a reminder" do
-      pending ("needs tests - Reminder.new.movie")
+      create_reminders(given_user, given_movie)
+      given_reminder.movie.should == given_movie
     end
 
     it "should get tomorrow's releases" do
-      pending ("needs tests - Reminder.tomorrows_releases")
+      create_reminders(given_user, given_movie)
+      given_movie.update_attributes(release_date: Date.tomorrow)
+      Reminder.tomorrows_releases.should == [given_reminder]
+    end
+
+    it "should destroy the reminders for user if user is destroyed" do
+      create_reminders(given_user, given_movie)
+      given_user.reminders.count.should == 1
+      given_user.destroy
+      Reminder.count.should == 0
     end
 
   end
