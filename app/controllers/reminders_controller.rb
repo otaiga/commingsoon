ca615ebdda7c23e9c838830b7d4fc2a855364dbd
otaiga <karl.turner5@gmail.com>
2013-01-19 10:21:40 +0000

    added notifications for adding and removing reminders

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -10,14 +10,18 @@ class RemindersController < ApplicationController
     movie_title_id = params[:title_id]
     reminder = current_user.reminders.new(movie_title_id: movie_title_id)
     if reminder.save
-      redirect_to root_path
+      redirect_to root_path, :notice => "The movie has been added to your reminders list"
+    else
+      redirect_to root_path, :alert => "There was an issue adding the movie to your reminders list"
     end
   end
 
   def destroy
     reminder = current_user.reminders.find(params[:id])
     if reminder.destroy
-      redirect_to reminders_path
+      redirect_to reminders_path, :notice => "Your reminder has been removed"
+    else
+      redirect_to reminders_path, :alert => "There was an issue removing your reminder"
     end
   end
 
