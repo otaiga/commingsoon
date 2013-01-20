c46c0fa82f4428a3ff521624acac1a28933c1686
richlewis14 <richlewis14@gmail.com>
2013-01-20 17:11:06 +0000

    CSS changes, added text to emails

--- app/mailers/moviereminder.rb
+++ app/mailers/moviereminder.rb
@@ -2,6 +2,7 @@ class Moviereminder < ActionMailer::Base
   default from: "from@example.com"
 
   def send_reminder(movie_name, recipient)
+    @user = user
     @movie_name = movie_name
     mail(:to => recipient, :subject => "Your Movie Reminder for Tomorrow's Release").deliver
   end
