f978bd9f627a52fe29508a3b1ddd84477c25b6f9
richlewis14 <richlewis14@gmail.com>
2013-01-22 19:13:54 +0000

    scroll upwards

---
+++ app/assets/javascripts/top.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+        // scroll body to 0px on click
+        $('#up').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+});
